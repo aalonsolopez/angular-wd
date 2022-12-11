940dcdca081a9b47de378bacc7b4a460e79fd0f1
Alejandro Alonso López <aalonsopersonal@gmail.com>
2022-12-11 21:38:35 +0100

    checkpoint1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { PreferencesComponent } from './pages/preferences/preferences.component';
+import { RecordsComponent } from './pages/records/records.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainPageComponent,
+    PreferencesComponent,
+    RecordsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
